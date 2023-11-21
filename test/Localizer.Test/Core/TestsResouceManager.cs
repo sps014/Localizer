63cd5101e9ea061df1d6602c1d831e87ed123284
Shivendra  Singh <45932883+sps014@users.noreply.github.com>
2023-11-21 11:52:34 +0530

    test set Value and get value

--- test/Localizer.Test/Core/TestsResouceManager.cs
+++ test/Localizer.Test/Core/TestsResouceManager.cs
@@ -13,6 +13,7 @@ namespace Localizer.Test.Core
     {
         ResxManager manager;
         TestsResourceWriterReader readerWriter;
+        ResxFileSystemLeafNode leaf;
 
         public TestsResouceManager()
         {
@@ -26,21 +27,19 @@ namespace Localizer.Test.Core
             readerWriter.AddResxStrings("");
             readerWriter.AddResxStrings("de",false);
             readerWriter.AddResxStrings("fr", false);
-        }
-        [Fact]
-        public void BuildResxManager()
-        {
-            Init();
+
             ResxResourceWriter writer = new ResxResourceWriter(Consts.OUT_PATH.JoinPath("Dir").JoinPath("Properties.resx"));
             File.WriteAllText(Consts.OUT_PATH.JoinPath("Dir").JoinPath("PR0.csproj"), string.Empty);
 
             manager = new ResxManager(Consts.OUT_PATH);
             manager.BuildCollectionAsync(new CancellationTokenSource()).GetAwaiter().GetResult();
-            TestTree(manager.Tree);
         }
 
-        private void TestTree(ResxLoadDataTree tree)
+        [Fact]
+        public void TestTree()
         {
+            Init();
+            var tree= manager.Tree;
             Assert.NotNull(tree);
             Assert.Equal("TestOut",tree.Root!.NodeName);
             Assert.False(tree.Root!.IsCSharpProjectDirectory);
@@ -52,18 +51,80 @@ namespace Localizer.Test.Core
             Assert.True(resource!.IsLeafResXFileNode);
             Assert.Equal("Properties", resource!.NodeName);
 
-            var leaf = tree.Root.ChildrenCollection.Last() as ResxFileSystemLeafNode;
-            TestLeafCulturesEntity(leaf!);
+            leaf = tree.Root.ChildrenCollection.Last() as ResxFileSystemLeafNode;
         }
 
-        private void TestLeafCulturesEntity(ResxFileSystemLeafNode  leaf)
+        [Fact]
+        public void TestLeafCulturesEntity()
         {
+            if (leaf == null)
+                TestTree();
+
             var cultures = leaf.ResxEntry.Cultures.OrderBy(x=>x).ToList();
             Assert.Equal(3, cultures.Count);
             Assert.Equal("", cultures[0]);
             Assert.Equal("de", cultures[1]);
             Assert.Equal("fr", cultures[2]);
 
+        }
+
+        [Fact]
+        public void TestLeafKeyOfEntity()
+        {
+            if (leaf == null)
+                TestTree();
+            var keys = leaf.Keys.ToList();
+            Assert.Equal(3, keys.Count);
+            Assert.Equal("k2", keys[0]);
+            Assert.Equal("k3", keys[1]);
+            Assert.Equal("k4", keys[2]);
+        }
+
+        [Theory]
+        [InlineData("de")]
+        [InlineData("fr")]
+        [InlineData("")]
+
+        public void TestLeafValueOfEntity(string culture)
+        {
+            if (leaf == null)
+                TestTree();
+            var keys = leaf.Keys.ToList();
+            Assert.Equal(3, keys.Count);
+            Assert.Equal(string.Empty, leaf.ResxEntry.GetValue(keys[0],culture));
+            Assert.Equal("v3", leaf.ResxEntry.GetValue(keys[1], culture));
+            Assert.Equal("v4", leaf.ResxEntry.GetValue(keys[2], culture));
+        }
+        [Theory]
+        [InlineData("de")]
+        [InlineData("fr")]
+        [InlineData("")]
+
+        public void TestLeafSetValueOfEntity(string culture)
+        {
+            if (leaf == null)
+                TestTree();
+            var keys = leaf.Keys.ToList();
+            Assert.Equal(3, keys.Count);
+
+            leaf.ResxEntry.SetValue(keys[0],"v2", culture);
+            leaf.ResxEntry.SetValue(keys[1], null, culture);
+
+            Assert.Equal("v2", leaf.ResxEntry.GetValue(keys[0], culture));
+            Assert.Null(leaf.ResxEntry.GetValue(keys[1], culture));
+            Assert.Equal("v4", leaf.ResxEntry.GetValue(keys[2], culture));
+
+            leaf.ResxEntry.TryGetFilePath(culture,out string path);
+
+            var recs = new ResxResourceReader(path).ToList();
+
+            var o = recs.First(x => x.Key == "k2");
+            Assert.Equal("v2", o.Value);
+
+            o = recs.First(x => x.Key == "k3");
+            Assert.Equal(string.Empty, o.Value);
+
+            Assert.Equal(3, recs.Count);
         }
     }
 }
